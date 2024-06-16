75b60ae8a014529df717b30f396957c59fd03f3e
mark-eskander <119497072+mark-eskander@users.noreply.github.com>
2024-06-16 20:47:35 +0300

    hide key

--- lama.py
+++ lama.py
@@ -38,27 +38,4 @@ if __name__== '__main__' :
     uvicorn.run(app,host='127.0.0.1',port=8000)
 
 
-# client = OpenAI(
 #     api_key="LL-1RC7LxETXTYzdwml76VfaP4ynaZunA7vsXXOTL9n0gMzuGB2VnJgb85xhAMKmlnv",
-#     base_url="https://api.llama-api.com"
-# )
-
-# response = client.chat.completions.create(
-#     model="llama-13b-chat",
-#     messages=[
-#         {"role": "system", "content": "translate to english then Mention positive as the first word if the next sentiment is positive otherwise write negative the first word if the sentiment is negative "},
-#         {"role": "user", "content": input("Write your review: ") }   
-
-#     ]
-# )
-
-# review = response.choices[0].message.content
-
-
-# def check_for_positive(review):
-#     if "positive" in review.lower():
-#         print("positive review")
-#     else:
-#         print("negative review")
-        
-# check_for_positive(review)
